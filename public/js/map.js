0304bb19f41203b03cb86e60ddb42f762505c55e
Proskurina <yproskurina@yahoo.com>
2015-12-15 15:24:16 +0000

    Map and opportunities are built around you

--- public/js/map.js
+++ public/js/map.js
@@ -4,27 +4,43 @@ $.getJSON(connectedLink, function showOps(data){
 
   organizations = data.data.items
 
-  var myLatlng = new google.maps.LatLng(51.567526, -0.182308);
-  var mapOptions = {
-    zoom: 12,
-    center: myLatlng
+
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
   }
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  for (i=0; i<organizations.length; i++){
-    (function placeMarker (i){
-      lat = JSON.stringify(data.data.items[i].lat)
-      lng = JSON.stringify(data.data.items[i].lng)
-      title = JSON.stringify(data.data.items[i].title)
+  function showPosition(position) {
+    myLat = position.coords.latitude
+    myLng = position.coords.longitude;
 
-      var myLatlng = new google.maps.LatLng(lat, lng);
 
-      var marker = new google.maps.Marker({
-          position: myLatlng,
-          title: title
-      });
-      marker.setMap(map);
-    })(i);
+    var myLatlng = new google.maps.LatLng(myLat, myLng);
+    var mapOptions = {
+      zoom: 12,
+      center: myLatlng
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    for (i=0; i<organizations.length; i++){
+      (function placeMarker (i){
+        lat = JSON.stringify(data.data.items[i].lat)
+        lng = JSON.stringify(data.data.items[i].lng)
+        title = JSON.stringify(data.data.items[i].title)
+
+        var myLatlng = new google.maps.LatLng(lat, lng);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            title: title
+        });
+        marker.setMap(map);
+      })(i);
+    };
   };
 
+  getLocation()
 });
